6ea1ced10c3c60cf07c7228a466ac1db7b9f1e37
Narek <narek.okroyan@fluxtechs.com>
2023-05-17 01:27:00 +0400

    added new things

--- train_restoration.py
+++ train_restoration.py
@@ -66,7 +66,6 @@ def psnr_loss(original, restored, max_val=1.0):
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-torch.distributed.init_process_group(backend='nccl')
 model = nn.DataParallel(model)
 model.to(device)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
